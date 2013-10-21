f4676c487ca239268f94d1fd0a0195c3c702e4c9
Oliver Tupman <oliver.tupman@centralway.com>
2013-10-21 13:36:35 +0200

    Ensures we tell the API that it's a JSON object we're sending

--- js/app.js
+++ js/app.js
@@ -11,9 +11,10 @@ angular.module('JazoonDemoApp',
     $locationProvider.html5Mode(false);
   }])
   .config(['$httpProvider', function($httpProvider) {
+    // Ensuring that we always request JSON
     $httpProvider.defaults.headers.common = { 'Accept': 'application/json'};
     $httpProvider.defaults.headers.get  = { 'Accept': 'application/json' };
-    $httpProvider.defaults.headers.post = { 'Accept': 'application/json' };
+    $httpProvider.defaults.headers.post = { 'Accept': 'application/json', 'Content-Type': 'application/json' };
     $httpProvider.defaults.headers.options = { 'Accept': 'application/json' };
   }])
 ;  
